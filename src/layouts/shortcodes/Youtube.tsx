db086f6bea0c4d58a72a19c2264ff123f4da7307
Pedro Guimarães <pedroguimarraes@gmail.com>
2025-06-06 00:55:14 -0300

    fix

--- src/layouts/shortcodes/Youtube.tsx
+++ src/layouts/shortcodes/Youtube.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import React, { useEffect, useState } from "react";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 const Youtube = ({
@@ -11,6 +10,18 @@ const Youtube = ({
   title: string;
   [key: string]: any;
 }) => {
+  const [LiteYouTubeEmbed, setLiteYouTubeEmbed] = useState<any>(null);
+
+  useEffect(() => {
+    import("react-lite-youtube-embed").then((module) => {
+      setLiteYouTubeEmbed(() => module.default);
+    });
+  }, []);
+
+  if (!LiteYouTubeEmbed) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <LiteYouTubeEmbed
       wrapperClass="yt-lite rounded-lg"
